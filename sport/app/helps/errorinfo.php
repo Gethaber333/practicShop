c6024da2189676188d3b3eaa3e8ff600922a29aa
NutPrimuss <87525698+Gethaber333@users.noreply.github.com>
2023-07-05 11:47:10 +0300

    Delete sport directory

--- sport/app/helps/errorinfo.php
+++
@@ -1,5 +0,0 @@
-<?php if(count($errMsg)>0):?>
-    <?php foreach ($errMsg as $error):?>
-        <li><?=$error;?></li>
-        <?php endforeach;?>
-        <?php endif?>
--- sport/path.php
+++
@@ -1,5 +0,0 @@
-<?php
-const SITE_ROOT = __DIR__;
-const BASE_URL = "http://localhost/sport/";
-define("ROOT_PATH", realpath(dirname(__FILE__)));
-?> 
